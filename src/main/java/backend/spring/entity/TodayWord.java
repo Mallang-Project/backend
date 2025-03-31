11b0edddb77c535a444e22e75cb2185700bbcfcd
MinjiSeo16 <mjlove6630@naver.com>
2025-04-01 00:16:35 +0900

    [feat] - TodayWord 엔티티 추가

---
+++ src/main/java/backend/spring/entity/TodayWord.java
@@ -0,0 +1,35 @@
+package backend.spring.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "today_word")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TodayWord {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "word_id", nullable = false)
+	private Long id;
+
+	@Column(nullable = false)
+	private String emotion;
+
+	@Column(nullable = false)
+	private String style;
+
+	@Column(nullable = false)
+	private String tone;
+
+	@Column(columnDefinition = "TEXT", nullable = false)
+	private String word;
+}
