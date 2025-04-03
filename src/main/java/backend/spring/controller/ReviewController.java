929a7d2b1aef8117d69feb0216fbea32653d58c8
MinjiSeo16 <mjlove6630@naver.com>
2025-04-03 19:47:51 +0900

    [feat] - 관리자 리뷰 조회

--- src/main/java/backend/spring/controller/ReviewController.java
+++ src/main/java/backend/spring/controller/ReviewController.java
@@ -1,13 +1,16 @@
 package backend.spring.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import backend.spring.dto.request.ReviewRequestDto;
+import backend.spring.dto.response.ReviewListResponseDto;
 import backend.spring.service.ReviewService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,11 +23,19 @@ public class ReviewController {
 	private final ReviewService reviewService;
 
 	@PostMapping("/submit/{visitorId}")
-	public ResponseEntity createReview(
+	public ResponseEntity<Void> createReview(
 		@PathVariable Long visitorId,
 		@RequestBody @Valid ReviewRequestDto requestDto) {
 		reviewService.submitReview(visitorId, requestDto);
 		return ResponseEntity.status(201).build();
 	}
 
+	@GetMapping("/list")
+	public ResponseEntity<ReviewListResponseDto> getReviewList(
+		@RequestParam(defaultValue = "0") int page,
+		@RequestParam(defaultValue = "10") int size
+	) {
+		ReviewListResponseDto responseDto = reviewService.getReviewList(page, size);
+		return ResponseEntity.ok(responseDto);
+	}
 }
--- src/main/java/backend/spring/repository/VisitorRepository.java
+++ src/main/java/backend/spring/repository/VisitorRepository.java
@@ -1,8 +1,16 @@
 package backend.spring.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import backend.spring.entity.Visitor;
 
 public interface VisitorRepository extends JpaRepository<Visitor, Long> {
+
+	Page<Visitor> findAllByReviewIsNotNull(Pageable pageable);
+
+	@Query("SELECT COALESCE(AVG(v.score), 0) FROM Visitor v WHERE v.review IS NOT NULL")
+	double getAverageScore();
 }
