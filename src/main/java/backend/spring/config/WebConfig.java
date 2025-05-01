f5e7db45218b5a1dae8f5c4e7b8de9ba93f19756
MinjiSeo16 <mjlove6630@naver.com>
2025-05-01 15:47:46 +0900

    [fix] - CORS 설정 변경

--- src/main/java/backend/spring/config/WebConfig.java
+++ src/main/java/backend/spring/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/api/**")
-					.allowedOriginPatterns("https://malang-delta.vercel.app")
+					.allowedOriginPatterns("https://fronted-ebon.vercel.app")
 					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 					.allowedHeaders("*")
 					.allowCredentials(false);
