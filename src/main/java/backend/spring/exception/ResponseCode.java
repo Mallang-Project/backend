38d53912cb91fd6989eccdb252e8f698ed476213
eunseo <es7837@naver.com>
2025-05-29 22:46:42 +0900

    [refactor] - ResponseCode message를 한글로 통일

--- src/main/java/backend/spring/exception/ResponseCode.java
+++ src/main/java/backend/spring/exception/ResponseCode.java
@@ -7,8 +7,8 @@ public enum ResponseCode {
 	USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
 	MOVIE_NOT_FOUND(404, "영화를 찾을 수 없습니다."),
 	NO_RECOMMENDATION_FOUND(404, "조건에 맞는 영화가 없습니다."),
-	INVALID_FORMAT(400, "Invalid input format."),
-	OPENAI_LIMIT(400, "GPT API usage exceeded.");
+	INVALID_FORMAT(400, "잘못된 입력 형식 입니다."),
+	OPENAI_LIMIT(400, "gpt api의 사용량을 초과했습니다.");
 
 	private final int status;
 	private final String message;
